4cff364ae14712db63eb629cd99e7f6f0ffb9cbc
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-09-24 00:29:21 +0000

    add

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,11 @@
-console.log("start server")
+const express = require('express');
+const app = express();
+const port = 3001;
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello World' });
+});
+
+app.listen(port, () => {
+    console.log(`App is listening at http://localhost:${port}`);
+});

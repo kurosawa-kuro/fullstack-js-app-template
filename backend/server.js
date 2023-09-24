3ae233f7ed05918bdfaae6c5e2073c33aba0639b
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-09-24 00:55:49 +0000

    add

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import { app } from "./index.js";
 
 const port = process.env.PORT || 3001;
-console.log(process.env.PAYPAL_CLIENT_ID)
+console.log(process.env.DEVELOPE_DB_HOST)
 
 app.listen(port, () =>
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
